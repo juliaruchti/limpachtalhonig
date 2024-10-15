105a374663787faa00261b2fdc122fb8db706c9e
julia ruchti <127301814+juliaruchti@users.noreply.github.com>
2024-10-15 13:00:23 +0000

    alles reinkopiert über uns und warenkorb fehlt noch

--- index.js
+++ index.js
@@ -17,6 +17,18 @@ app.get("/impressum", async function (req, res) {
   res.render("impressum", {});
 });
 
+app.get("/produkte", async function (req, res) {
+  res.render("produkte", {});
+});
+
+app.get("/ueber_uns", async function (req, res) {
+  res.render("ueber_uns", {});
+});
+
+app.get("/warenkorb", async function (req, res) {
+  res.render("warenkorb", {});
+});
+
 /* Wichtig! Diese Zeilen müssen immer am Schluss der Website stehen! */
 app.listen(3010, () => {
   console.log(`Example app listening at http://localhost:3010`);
